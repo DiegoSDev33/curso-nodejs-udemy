f3d97e8069e757859ce947d0eb6ce0271be8806e
Diego Santos <diegodev.silva@gmail.com>
2023-12-26 22:59:38 -0300

    resgatando valores do banco de dados

---
+++ FundamentosNodejs/SECAO_10_MVC/7_Resgatando_itens/controllers/TaskController.js
@@ -0,0 +1,33 @@
+const { raw } = require('express')
+const Task = require('../models/Task')
+
+module.exports = class TaskController{
+
+  static createTask(req, res){
+    res.render('tasks/create')
+  }
+  
+  static async createSave(req,res){
+
+    const task ={
+      title: req.body.title,
+      description: req.body.description, 
+      done: false
+    }
+
+    await Task.create(task)
+    res.redirect('/tasks')
+  }
+
+  static async showTasks(req, res){
+
+    const tasks = await Task.findAll({raw: true})
+
+
+
+
+    res.render('tasks/all', {tasks})
+  }
+
+
+}
