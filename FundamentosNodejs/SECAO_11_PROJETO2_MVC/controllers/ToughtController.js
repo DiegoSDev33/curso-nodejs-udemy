851b388459455c71edf84b5c27c4339e1f45de07
Diego Santos <diegodev.silva@gmail.com>
2024-01-05 19:38:27 -0300

    funcao de buscar os pensamentos do banco estao funcionando!

--- FundamentosNodejs/SECAO_11_PROJETO2_MVC/controllers/ToughtController.js
+++ FundamentosNodejs/SECAO_11_PROJETO2_MVC/controllers/ToughtController.js
@@ -7,9 +7,34 @@ module.exports = class ToughtController {
   }
 
   static async dashboard(req, res) {
-    res.render("toughts/dashboard");
+    const userId = req.session.userid;
+  
+    try {
+      const user = await User.findOne({
+        where: {
+          id: userId
+        },
+        include: Tought // Verifique se o nome do modelo está correto aqui
+      });
+  
+      if (!user) {
+        return res.redirect('/login');
+      }
+  
+      const toughts = user.Toughts.map((result) => result.dataValues);
+      console.log(toughts);
+  
+      res.render("toughts/dashboard", { toughts });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Erro ao carregar o dashboard.');
+    }
   }
+  
 
+
+
+  
   static createTought(req, res) {
     res.render("toughts/create");
   }
