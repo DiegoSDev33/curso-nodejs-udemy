e391a6d5b9a87ebf587d7d8155fcb80658caa6b1
Diego Santos <diegodev.silva@gmail.com>
2024-01-07 02:17:30 -0300

    criado funcao de atualizar pensamento

--- FundamentosNodejs/SECAO_11_PROJETO2_MVC/routes/toughtsRoutes.js
+++ FundamentosNodejs/SECAO_11_PROJETO2_MVC/routes/toughtsRoutes.js
@@ -7,6 +7,8 @@ const ToughtController = require('../controllers/ToughtController')
 const checkAuth  = require('../helpers/auth').checkAuth 
 
 router.get('/add', checkAuth, ToughtController.createTought)
+router.get('/edit/:id', checkAuth, ToughtController.updateTought)
+router.post('/edit', checkAuth, ToughtController.updateToughtSave)
 router.post('/add', checkAuth, ToughtController.createToughtSave)
 router.get('/dashboard',checkAuth, ToughtController.dashboard)
 router.post('/remove', checkAuth, ToughtController.removeTought)
